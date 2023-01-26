c06a5685c2e0718549d5c85413621c560c5c1287
Eliyas <eliyas.kapasi@gmail.com>
2023-01-26 23:39:42 +0000

    the save button for 9am now works

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 let timeDisplayEl = document.querySelector("#currentDay");
 let timeBlockEl = document.querySelector("textarea")
 let saveButtonEl = document.querySelector("#nineAM-save")
+let hourlyPlan = document.querySelector(".plan")
 
 
 
 
 timeStamp();
 colouredTabs();
+renderPlans();
 
 
 saveButtonEl.addEventListener("click", storePlans);
@@ -22,26 +24,39 @@ function timeStamp() {
 
 // This part determines if a textarea is in the future or the past.
 function colouredTabs() {
-    let currentHour = moment().format("H");
+    let currentHour = moment().hour();
     console.log(currentHour)
     let nineAM = timeBlockEl.getAttribute("id")
     console.log(nineAM)
 
     if (currentHour > nineAM) {
-        timeBlockEl.classList.add("past")
-    }
-    if (currentHour == nineAM) {
-        timeBlockEl.classList.add("present")
+        timeBlockEl.classList.remove("future");
+        timeBlockEl.classList.remove("present");
+        timeBlockEl.classList.add("past");
         
-    } else {
-        timeBlockEl.classList.add("future")       
+        
     }
+    else if (currentHour == nineAM) {
+        timeBlockEl.classList.add("present");
+        timeBlockEl.classList.remove("future");
+        timeBlockEl.classList.remove("past");
+        
+    } 
+    else {
+        timeBlockEl.classList.add("future");    
+        timeBlockEl.classList.remove("present");
+        timeBlockEl.classList.remove("past");
+          
+    } 
 }
 
 
 function storePlans(event) {
     
     console.log("yeet")
+    let userPlan = hourlyPlan.value
+    console.log(userPlan)
+    localStorage.setItem("userPlan", JSON.stringify(userPlan));
 
     event.preventDefault();
 }
@@ -51,7 +66,9 @@ function storePlans(event) {
 
 
 function renderPlans() {
-    let storedPlans = JSON.parse(localStorage.getItem("9am-plan"))
+    let storedPlans = JSON.parse(localStorage.getItem("userPlan"))
+    console.log(storedPlans)
+    timeBlockEl.textContent = storedPlans;
 
     
     
