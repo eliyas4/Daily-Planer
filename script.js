58f4a94a5ac0aa3f5940de88f08e6bb6c0c9c1c7
Eliyas <eliyas.kapasi@gmail.com>
2023-01-27 21:24:07 +0000

    event delegation now works so all the save buttons are functional

--- script.js
+++ script.js
@@ -2,17 +2,25 @@ let timeDisplayEl = document.querySelector("#currentDay");
 let timeBlockEl = document.querySelectorAll("textarea")
 let nineAMSave = document.querySelector("#nineAM-save")
 let hourlyPlan = document.querySelectorAll(".plan")
+
 let arrayStore = []
 timeStamp();
 colouredTabs();
 renderPlans();
+
+// Event delegation is used here to trigger the storePlans function
+window.addEventListener("click", function(event) {
+    element = event.target;
+    if (element.matches("button") === true) {
+        storePlans(); 
+    }
+})
 
 // This part displays the time in the header.
 function timeStamp() {
     let timeStamp = moment().format("dddd, MMMM Do" )
     timeDisplayEl.textContent = timeStamp; 
 }
-
 
 // This part determines if a textarea is in the future or the past.
 function colouredTabs() {
@@ -21,20 +29,16 @@ function colouredTabs() {
 
     for (let i = 0; i < timeBlockEl.length; i++) {
         let blockTime = timeBlockEl[i].getAttribute("id")
-        console.log(blockTime)
 
         if (currentHour > blockTime) {
            timeBlockEl[i].classList.remove("future");
            timeBlockEl[i].classList.remove("present");
-           timeBlockEl[i].classList.add("past");
-            
-            
+           timeBlockEl[i].classList.add("past");           
         }
         else if (currentHour == blockTime) {
            timeBlockEl[i].classList.add("present");
            timeBlockEl[i].classList.remove("future");
-           timeBlockEl[i].classList.remove("past");
-            
+           timeBlockEl[i].classList.remove("past");            
         } 
         else {
            timeBlockEl[i].classList.add("future");    
@@ -44,47 +48,19 @@ function colouredTabs() {
     }   
 }
 
-nineAMSave.addEventListener("click", storePlans);
-
-
-function storePlans(event) {    
-    console.log("working")
-    console.log(hourlyPlan)
-    
+// This stores the plans when the save button is pressed
+function storePlans() {    
     for (let i = 0; i < hourlyPlan.length; i++) {
         const textAreaVal = hourlyPlan[i].value;
-        console.log(textAreaVal)
-        arrayStore.push(textAreaVal)
-        
-        
+        arrayStore.push(textAreaVal) 
     }
-    console.log(arrayStore)
-//    let userPlan = hourlyPlan.value
-//    console.log(userPlan)
     localStorage.setItem("userPlan", JSON.stringify(arrayStore));
-    event.preventDefault();
 }
 
+// This renders any plans that are already in arrayStore
 function renderPlans() {
     let storedPlans = JSON.parse(localStorage.getItem("userPlan"))
-//    console.log(storedPlans[1])
-//    console.log(hourlyPlan[1])
-    console.log(storedPlans[0])
-    console.log(hourlyPlan[0])
-    
-
-
     for (let i = 0; i < 9; i++) {
-        hourlyPlan[i].textContent = storedPlans[i];
-        
+        hourlyPlan[i].textContent = storedPlans[i];     
     }
-        
-        
-
-        
-    
-//    let storedPlans = JSON.parse(localStorage.getItem("userPlan"))
-//    console.log(storedPlans)
-//    let nineamBlock = document.querySelector(".nine")
-//    nineamBlock.textContent = storedPlans;
 }
